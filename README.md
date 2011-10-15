c442c5ecbe2c6df2761a213f4eb6b1d68240ac75
Mike Tahani <m.tahani@gmail.com>
2011-10-15 14:20:45 -0700

    compressed code & made it faster

--- README.md
+++ README.md
@@ -4,24 +4,26 @@ Last month, someone asked the NICAR mailing list about scraping a list of lobbyi
 
 > "Can anyone offer pointers or a good tutorial on building a web scraper for someone who has never done this before? I want to rustle all the fields from here -- http://bit.ly/pBE5Ph - into one file without having to copy/paste my way through 400 pages."
 
-The website is [here](http://apps.sd.gov/applications/ST12ODRS/LobbyistViewlist.asp).
-
 I wrote a scraper to collect the data, and recently updated it to reflect best-practices. Hopefully, journalists and other data nerds will find this useful.
+
+## Dependencies
+
+Requires BeautifulSoup (`easy_install BeautifulSoup` on Windows).
 
 ## Files
 
 *docs/* contains the scraped pages as of Sept 2011.
 
-### Old Version
-*old.py* is the initial version of the code. Despite using some bad practices (parsing HTML with regex), it's _a lot_ faster than the new parser, *parse_records.py*. This will be fixed soon. *old.py* contains code to both scrape and parse the data, whereas the new version splits the tasks into separate scripts.
-
-The output for *old.py* is in *lobbyists_old.csv*.
-
 ### New Version
 
-*scrape_records.py* will scan the site, collecting all records. It's pretty ugly since it's just a copy/paste from *old.py*. 
+*scrape_records.py* will scan the site, collecting all records. It's pretty ugly since it's just a copy/paste from *old.py*. *parse_records.py* is the parser.
 
-Output is in *lobbyists_new.csv*. Optionally, you can return a JSON-formatted string of the data from the script, piping it to a .json file. See *parse_records.py* for usage information.
+Output is in *lobbyists_new.csv*. <del>Optionally, you can return a JSON-formatted string of the data from the script, piping it to a .json file. See *parse_records.py* for usage information.</del> *dropped*
+
+### Old Version
+*old.py* is the initial version of the code. Despite using some bad practices (parsing HTML with regex), it's _a lot_ faster than the new parser, *parse_records.py*. This is owing to BeautifulSoup's overhead. *old.py* contains code to both scrape and parse the data, whereas the new version splits the tasks into separate scripts.
+
+The output for *old.py* is in *lobbyists_old.csv*.
 
 ## Author Info
 Mike Tahani, m.tahani at gmail
--- parse_records.py
+++ parse_records.py
@@ -1,60 +1,33 @@
 # parse records from the local copies of pages from http://apps.sd.gov/applications/ST12ODRS/LobbyistViewlist.asp
 # mike tahani   m.tahani at gmail
-
 from BeautifulSoup import *
-import re, json, csv, os, sys
+import csv, os
 
-def load_file(filename):
-    """ open a file and make it a BS object """
-    f = open(filename).read()
-    return BeautifulSoup(f)
+def gen_readfiles(dir):
+    writer = csv.writer( open('lobbyists_new1.csv', 'wb'), dialect='excel' )
+    writer.writerow( ['year','name','address','address2',
+                      'employer','employer_address',
+                      'employer_address2'] )
+    for soup in read_files_as_soup(dir):
+        table = soup.findAll( 'tr' , attrs={ 'bgcolor' : '#FFFFFF' } )
+        records = ( record.contents for record in table )
+        for record in records:
+            fields = [ get_innertext(node) for node in record if node != '\n' ]
+            writer.writerow(fields)
+        print "... done"
+    return
 
-def get_table_entries(soup):
-    """ get table entries by <tr> tag """
-    tags = soup.findAll( 'tr' , attrs={ 'bgcolor' : '#FFFFFF' } )   # find all tr tags with "bgcolor='#FFFFFF'"
-    tag_contents = [ tag.contents for tag in tags ]                 # get tag contents (fields)
-    hdrs = ['year','name','address','address2','employer','employer_address',
-            'employer_address2']    # headers
-    entries = []    # collection of fields
-    for entry in tag_contents:
-        # get the inner text of the BS nodes found in each table entry:
-        fields = [get_innertext(node) for node in entry if node != '\n']
-        # add appropriate headers, append entry data to master entry collection
-        entries.append( dict( zip( hdrs, fields ) ) ) 
-    return entries
-
-def get_all_entries(dir):
-    """ get all entries from all local copies of tables in dir """
-    all_entries = []    # collection of all fields from all table entries
-    for page in os.listdir(dir):
-        print "processing", page
-        # collect entries from each table on each page
-        all_entries += get_table_entries( load_file( dir + page ) )
-    return all_entries
-
+def read_files_as_soup(dir):
+    """ read files from a dir, process them as BS objects """
+    for filename in os.listdir(dir):
+        print "processing", filename,
+        yield BeautifulSoup( open( dir + filename ).read() )
+        
 def get_innertext(node):
     """ get the inner text of a BS node, get rid of spaces """
     innertext = ''.join(node.findAll(text=True))
     return re.sub('&nbsp;?', '', innertext)
 
-def output_csv(entries, filename):
-    """ write csv output of entries to filename; expects a list of dicts """
-    hdrs = ['year','name','address','address2','employer','employer_address',
-            'employer_address2']    # headers
-    # open a csv dict writer object for excel formatting:
-    writer = csv.DictWriter(open(filename, 'wb'), hdrs, dialect='excel')
-    # write the headers (must pass it as a dict to DictWriter):
-    writer.writerow( dict( (h,h) for h in hdrs ))
-    for entry in entries:
-        # write all entries to the csv file
-        writer.writerow(entry)
-    return
-
-def output_json(entries):
-    """ print json output of entries; expects a python object """
-    return json.dumps(entries)
-
 if __name__ == '__main__':
     
-    output_csv(get_all_entries('docs/'), 'lobbyists_new.csv')
-    #output_json(get_all_entries('docs/')) # usage: pipe the output to a .json file
+    gen_readfiles('docs/')
