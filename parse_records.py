55191c810ba48f7e239d4335f925e4b416ccb644
Mike Tahani <m.tahani@gmail.com>
2011-10-19 14:28:48 -0700

    added comments

--- parse_records.py
+++ parse_records.py
@@ -4,16 +4,20 @@ from BeautifulSoup import *
 import csv, os
 
 def html_to_csv(dir):
-    writer = csv.writer( open('lobbyists_new1.csv', 'wb'), dialect='excel' )
+    """ open directory of local SD-lobbyists pages and convert them to csv """
+    writer = csv.writer( open('lobbyists_new.csv', 'wb'), dialect='excel' )
     writer.writerow( ['year','name','address','address2',
                       'employer','employer_address',
-                      'employer_address2'] )
+                      'employer_address2'] )    # headers
     for soup in read_files_as_soup(dir):
+        # find all tr tags with a certain attribute:
         table = soup.findAll( 'tr' , attrs={ 'bgcolor' : '#FFFFFF' } )
+        # generator expression to get contents of all tr tags:
         records = ( record.contents for record in table )
         for record in records:
+            # get table fields from the records:
             fields = [ get_innertext(node) for node in record if node != '\n' ]
-            writer.writerow(fields)
+            writer.writerow(fields) # write the record's fields as a csv row
         print "... done"
     return
 
@@ -21,12 +25,13 @@ def read_files_as_soup(dir):
     """ read files from a dir, process them as BS objects """
     for filename in os.listdir(dir):
         print "processing", filename,
+        # convert local HTML page to a BS object:
         yield BeautifulSoup( open( dir + filename ).read() )
         
 def get_innertext(node):
     """ get the inner text of a BS node, get rid of spaces """
-    innertext = ''.join(node.findAll(text=True))
-    return re.sub('&nbsp;?', '', innertext)
+    innertext = ''.join(node.findAll(text=True))    # get text only, no tags
+    return re.sub('&nbsp;?', '', innertext) # get rid of some HTML spaces
 
 if __name__ == '__main__':
     
